a058c21e32e6c3bcbd0f188def0e7adb1f8a9869
henriqueboneto <h.boneto@outlook.com>
2021-12-16 02:57:18 -0300

    feat: create student builder

---
+++ src/school/student_builder.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from src.school.cpf import CPF
+from src.school.email import Email
+from src.school.phone import Phone
+from src.school.student import Student
+
+
+class StudentBuilder:
+    def __init__(self):
+        self._student: Optional[Student] = None
+
+    def with_name_cpf_email(self, name: str, cpf: str, email: str) -> 'StudentBuilder':
+        self._student = Student(name=name, cpf=CPF(cpf), email=Email(email))
+        return self
+
+    def with_phone(self, phone_ddd: str, phone_number: str) -> 'StudentBuilder':
+        self._student.add_phone(Phone(phone_ddd, phone_number))
+        return self
+
+    def build(self) -> Student:
+        return self._student
---
+++ src/tests/school/test_student_builder.py
@@ -0,0 +1,30 @@
+from src.school.cpf import CPF
+from src.school.email import Email
+from src.school.phone import Phone
+from src.school.student_builder import StudentBuilder
+
+def test_create_student():
+    name = "boneto"
+    cpf = "888.111.231-02"
+    email = "boneto@yahoo.com.br"
+    ddd_1 = "51"
+    number_1 = "123456789"
+    ddd_2 = "51"
+    number_2 = "12345678"
+    student_builder = StudentBuilder()
+    student = (student_builder
+    ).with_name_cpf_email(
+        name=name,
+        cpf=cpf,
+        email=email,
+    ).with_phone(
+        phone_ddd=ddd_1, 
+        phone_number=number_1,
+    ).with_phone(
+        phone_ddd=ddd_2,
+        phone_number=number_2,
+    ).build()
+    assert student.get_cpf() == CPF(cpf)
+    assert student.get_email() == Email(email)
+    assert student.get_name() == name
+    assert student.get_phones() == [Phone(ddd_1, number_1), Phone(ddd_2, number_2)]

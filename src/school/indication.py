ead0c58e13d06fd4d1542ec7be1aaa3a93dc7a79
henriqueboneto <h.boneto@outlook.com>
2021-12-16 02:23:38 -0300

    feat: indication date & getters

--- src/school/indication.py
+++ src/school/indication.py
@@ -1,7 +1,18 @@
 from src.school.student import Student
+from datetime import datetime
 
 
 class Indication:
     def __init__(self, nominating: Student, nominated: Student):
         self._nominating: Student = nominating
         self._nominated: Student = nominated
+        self._indication_date: datetime = datetime.now()
+
+    def get_nominating(self):
+        return self._nominating
+
+    def get_nominated(self):
+        return self._nominated
+
+    def get_indication_date(self):
+        return self._indication_date
